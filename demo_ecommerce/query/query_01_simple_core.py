0563518e35fc651113fb6d63bf709743d187fca9
Ettore Galli <ettore.galli@eng.it>
2024-05-07 23:03:07 +0200

    Group by

--- demo_ecommerce/query/query_01_simple_core.py
+++ demo_ecommerce/query/query_01_simple_core.py
@@ -8,12 +8,6 @@ from demo_ecommerce.query.query_base import render_query
 def query_data(connection_string: str):
     with create_session_maker(connection_string)() as session:
 
-        # Elenco clienti
-
-        # SELECT customer.id, customer.name, customer.address
-        # FROM customer
-        # WHERE customer.name LIKE '%%oni%%'
-
         query = select(Customer).where(Customer.name.like("%oni%"))
 
         print(render_query(session, query))
--- demo_ecommerce/query/query_02_group_core.py
+++ demo_ecommerce/query/query_02_group_core.py
@@ -1,18 +1,22 @@
+from sqlalchemy import func, select
 from demo_ecommerce.connection.connection import create_session_maker
 from demo_ecommerce.connection.connection_base import Connections
-from demo_ecommerce.models.models import Customer
+from demo_ecommerce.models.models import InvoiceDetail
 from demo_ecommerce.query.query_base import render_query
 
 
 def query_data(connection_string: str):
     with create_session_maker(connection_string)() as session:
 
-        query = session.query(Customer).filter(Customer.name.like("%oni%"))
+        # pylint: disable=not-callable
+        query = select(
+            InvoiceDetail.item_code, func.count(InvoiceDetail.id).label("occurrences")
+        ).group_by(InvoiceDetail.item_code)
 
         print(render_query(session, query))
 
-        for record in query.limit(5).all():
-            print(record.name, record.address)
+        for record in session.execute(query):
+            print(record.item_code, record.occurrences)
 
 
 if __name__ == "__main__":
